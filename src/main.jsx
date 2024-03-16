d9ef95bb4612334eb04943682c62610a3fa30846
ASSIMv3 <hassanassim01@gmail.com>
2024-03-16 15:51:39 +0000

    install react router and create the home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Sparkle Jewelry</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
