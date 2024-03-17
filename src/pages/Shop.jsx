4248dfc67c24ef820cbd9b73981dac6b4d038517
ASSIMv3 <hassanassim01@gmail.com>
2024-03-17 11:56:25 +0000

    adding product to shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,34 @@
 import "../styles/Products.css";
 import "../styles/Shop.css";
-import ShopNavbar from "../components/ShopNavbar";
 import Footer from "../components/Footer";
+import ShopNavbar from "../components/ShopNavbar";
+import SingleProduct from "../components/SingleProduct";
+import { useStateValue } from "../useStateValue";
 
 const Shop = () => {
+  const [{ products }] = useStateValue();
+
   return (
     <>
       <ShopNavbar />
+      <main>
+        <section id="shop">
+          <div className="container">
+            <h2 className="shop__title">Shop</h2>
+            <div className="products">
+              {products.map((product) => (
+                <SingleProduct
+                  key={product._id}
+                  id={product.id}
+                  name={product.name}
+                  price={product.price}
+                  image={product.img_url}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   );
