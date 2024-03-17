44653a44a6406b10ded070ba4c728f84a9646d87
ASSIMv3 <hassanassim01@gmail.com>
2024-03-17 11:54:10 +0000

    adding Footer to Shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,13 @@
 import "../styles/Products.css";
 import "../styles/Shop.css";
 import ShopNavbar from "../components/ShopNavbar";
+import Footer from "../components/Footer";
 
 const Shop = () => {
   return (
     <>
       <ShopNavbar />
+      <Footer />
     </>
   );
 };
