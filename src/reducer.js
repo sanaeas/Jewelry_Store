e049d744c26e100516d7a6bf9b4c79b50c2b8833
ASSIMv3 <hassanassim01@gmail.com>
2024-03-17 12:16:35 +0000

    expand global state management to include cart functionalities

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,61 @@
 export const initialState = {
+  cart: [],
   products: [],
 };
 
+// Selector
+export const getCartTotal = (cart) =>
+  cart?.reduce((amount, item) => item.price * item.quantity + amount, 0);
+
 export const reducer = (state, action) => {
   switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, action.item],
+      };
+
+    case "REMOVE_FROM_CART":
+      // eslint-disable-next-line no-case-declarations
+      const index = state.cart.findIndex(
+        (cartItem) => cartItem.id === action.id
+      );
+      // eslint-disable-next-line no-case-declarations
+      let newCart = [...state.cart];
+
+      if (index >= 0) {
+        newCart.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in cart!`
+        );
+      }
+
+      return {
+        ...state,
+        cart: newCart,
+      };
+
+    case "UPDATE_QUANTITY":
+      // eslint-disable-next-line no-case-declarations
+      const proIndex = state.cart.findIndex(
+        (element) => element.id === action.id
+      );
+      // eslint-disable-next-line no-case-declarations
+      let nCart = [...state.cart];
+      nCart[proIndex].quantity = action.quantity;
+
+      return {
+        ...state,
+        cart: nCart,
+      };
+
+    case "EMPTY_CART":
+      return {
+        ...state,
+        cart: [],
+      };
+
     case "SET_PRODUCTS":
       return {
         ...state,
