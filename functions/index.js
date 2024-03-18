512413960361bc1c9e4e2b74881b2cd71bff8889
sanaeas <sanaassim1@gmail.com>
2024-03-18 11:31:16 +0000

    Add route to fetch products

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,9 @@ const dbConnect = require('./config/dbConnect');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// routes
+const productsRoutes = require('./routes/products');
+
 app.use(express.json());
 
 dbConnect();
@@ -12,6 +15,8 @@ dbConnect();
 app.use('/', (req, res) => {
   res.send('Hello from the server side');
 });
+
+app.use('/api', productsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
