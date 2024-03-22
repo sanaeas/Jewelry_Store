ddb1e724aebe3eb9ecfe648a8e1442b2fda40005
sanaeas <sanaassim1@gmail.com>
2024-03-22 15:16:24 +0000

    User Authentication

---
+++ functions/controllers/AuthController.js
@@ -0,0 +1,53 @@
+const User = require('../models/User');
+const { createSecretToken } = require('../auth');
+const bcrypt = require('bcrypt');
+
+module.exports.Signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    const user = await User.create({ email, password, username });
+    const token = createSecretToken(user._id);
+    res.cookie('token', token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: 'User signed up successfully', success: true, user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: 'All fields are required' });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: 'Incorrect password or email' });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: 'Incorrect password or email' });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie('token', token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: 'User logged in successfully', success: true });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
