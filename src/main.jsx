6475135e355be995062898ce9f30e1a1362c6312
ASSIMv3 <hassanassim01@gmail.com>
2024-03-22 16:17:48 +0000

    adding new pages path

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-import { initialState, reducer } from "./reducer";
-import { StateProvider } from "./StateProvider";
+import Login from "./pages/Login";
+import ProductInfo from "./pages/ProductInfo";
+import Profile from "./pages/Profile";
 import Shop from "./pages/Shop";
 import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+import { initialState, reducer } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/shop",
-    element: <Shop />,
   },
   {
     path: "/signup",
@@ -28,8 +27,20 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
+    path: "/shop/:category?",
+    element: <Shop />,
+  },
+  {
+    path: "/products/:id",
+    element: <ProductInfo />,
+  },
+  {
     path: "/cart",
     element: <Cart />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ]);
 
