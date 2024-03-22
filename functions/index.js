25ae92bfea5ae537116b590a47e73d6879790016
sanaeas <sanaassim1@gmail.com>
2024-03-22 15:22:53 +0000

    Entry Point: define the app routes

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const dbConnect = require('./config/dbConnect');
-require('dotenv').config();
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-//routes
-const productsRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth');
-
-app.use(cors());
-app.use(express.json());
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const productsRoute = require('./routes/products');
+const ordersRoute = require('./routes/orders');
+const checkoutRoute = require('./routes/checkout');
+const authRoute = require('./routes/auth');
 
 dbConnect();
 
-app.use('/api', productsRoutes);
-
-app.use('/api', authRoutes);
-
-app.use('/', (req, res) => {
-  res.send('Hello from the server side');
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+// app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/', authRoute);
+app.use('/', checkoutRoute);
+app.use('/api', productsRoute);
+app.use('/api', ordersRoute);
